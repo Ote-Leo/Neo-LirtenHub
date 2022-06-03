3b799fcbe6413f3a0a8107f5fbaa481b6563b49f
Amir Ashraf <amir.weedy@gmail.com>
2022-06-03 14:41:14 +0200

    Initial ChatApp commit

---
+++ ChatApp/src/main/java/com/tau/chat/LirtenhubApplication.java
@@ -0,0 +1,13 @@
+package com.tau.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LirtenhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LirtenhubApplication.class, args);
+	}
+
+}
